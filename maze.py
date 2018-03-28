a15a94f65e16ecfd7ea8d4677d90f498edc096e0
Amelien Boussard <amelien.boussard@gmail.com>
2018-03-28 11:19:45 +0200

    add comment in maze.py and display the maze :yum:

--- maze.py
+++ maze.py
@@ -1,4 +1,3 @@
-import time
 import pygame
 from pygame.locals import *
 
@@ -7,6 +6,7 @@ class Maze:
         self.file = file
         self.setting = 0
 
+    #Generate the maze
     def generate(self):
         with open(self.file, "r") as file:
             level_setting = []
@@ -20,9 +20,10 @@ class Maze:
 
             self.setting = level_setting
 
+    #Function for display the game
     def display(self, window):
-        wall = pygame.image.load(img_wall).convert()
-        start = pygame.image.load(img_start).convert()
+        wall = pygame.image.load(img_wall).convert_alpha()
+        start = pygame.image.load(img_start).convert_alpha()
         guardian = pygame.image.load(img_guardian).convert_alpha()
 
         nbr_line = 0
@@ -41,28 +42,40 @@ class Maze:
             nbr_line +=1
 
 
+#Constant
 nbr_sprite_square = 15
 sprite_size = 30
 nbr_square = nbr_sprite_square * sprite_size
-
+#Customize the window
 title_window = "MAZE"
 img_ico = "images/brick.png"
-
+#Sprite of maze
 img_wall = "images/brick.png"
 img_start = "images/ether.png"
 img_guardian = "images/guardian.png"
-
+img_background = "images/background.jpg"
 
 pygame.init()
 
-window = pygame.display.set_mode((500, 500))
+#VAR
+game = 1
+window = pygame.display.set_mode((450, 450))
 ico = pygame.image.load(img_ico)
-pygame.display.set_icon(ico)
-pygame.display.set_caption(title_window)
-
 maze = 'maze.txt'
 level = Maze(maze)
-level.generate()
-level.display(window)
+background = pygame.image.load(img_background).convert()
 
-time.sleep(1)
+while game:
+    pygame.time.Clock().tick(30)
+    pygame.display.set_icon(ico)
+    pygame.display.set_caption(title_window)
+    level.generate()
+    window.blit(background, (0,0))
+    level.display(window)
+    #Refresh the display
+    pygame.display.flip()
+
+    #For leave the program
+    for event in pygame.event.get():
+        if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+            game = 0
