41d8a744436726cb4479e0c50956905f268b80db
Amelien Boussard <amelien.boussard@gmail.com>
2018-03-28 14:35:23 +0200

    add file for constant

---
+++ constant.py
@@ -0,0 +1,16 @@
+# Constant
+nbr_sprite_square = 15
+sprite_size = 30
+nbr_square = nbr_sprite_square * sprite_size
+maze = 'maze.txt'
+# Customize the window
+title_window = "MAZE"
+img_ico = "images/brick.png"
+# Sprite of maze
+img_wall = "images/brick.png"
+img_start = "images/ether.png"
+img_guardian = "images/guardian.png"
+img_background = "images/background.jpg"
+
+# Var
+game = True
--- maze.py
+++ maze.py
@@ -1,12 +1,14 @@
 import pygame
 from pygame.locals import *
+from constant import *
+
 
 class Maze:
     def __init__(self, file):
         self.file = file
         self.setting = 0
 
-    #Generate the maze
+    # Generate the maze
     def generate(self):
         with open(self.file, "r") as file:
             level_setting = []
@@ -20,7 +22,7 @@ class Maze:
 
             self.setting = level_setting
 
-    #Function for display the game
+    # Function for display the game
     def display(self, window):
         wall = pygame.image.load(img_wall).convert_alpha()
         start = pygame.image.load(img_start).convert_alpha()
@@ -33,35 +35,20 @@ class Maze:
                 x = nbr_square * sprite_size
                 y = nbr_line * sprite_size
                 if sprite == '#':
-                    window.blit(wall, (x,y))
+                    window.blit(wall, (x, y))
                 elif sprite == 'M':
-                    window.blit(start, (x,y))
+                    window.blit(start, (x, y))
                 elif sprite == 'G':
-                    window.blit(guardian, (x,y))
+                    window.blit(guardian, (x, y))
                 nbr_square += 1
-            nbr_line +=1
+            nbr_line += 1
 
-
-#Constant
-nbr_sprite_square = 15
-sprite_size = 30
-nbr_square = nbr_sprite_square * sprite_size
-#Customize the window
-title_window = "MAZE"
-img_ico = "images/brick.png"
-#Sprite of maze
-img_wall = "images/brick.png"
-img_start = "images/ether.png"
-img_guardian = "images/guardian.png"
-img_background = "images/background.jpg"
 
 pygame.init()
 
-#VAR
-game = 1
+# VAR
 window = pygame.display.set_mode((450, 450))
 ico = pygame.image.load(img_ico)
-maze = 'maze.txt'
 level = Maze(maze)
 background = pygame.image.load(img_background).convert()
 
@@ -70,12 +57,12 @@ while game:
     pygame.display.set_icon(ico)
     pygame.display.set_caption(title_window)
     level.generate()
-    window.blit(background, (0,0))
+    window.blit(background, (0, 0))
     level.display(window)
-    #Refresh the display
+    # Refresh the display
     pygame.display.flip()
 
-    #For leave the program
+    # For leave the program
     for event in pygame.event.get():
         if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-            game = 0
+            game = False
