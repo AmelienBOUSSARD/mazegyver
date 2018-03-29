f24c25977cc9c05521ed8db6eb16335dab36a3da
Amelien Boussard <amelien.boussard@gmail.com>
2018-03-29 09:55:29 +0200

    macgyver cut into another file

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from constant import *
+from macgyver import *
 
 
 class Maze:
@@ -45,58 +46,6 @@ class Maze:
                 nbr_square += 1
             nbr_line += 1
 
-
-class Macgyver:
-    """Class for create the character Macgyver"""
-
-    def __init__(self, right, left, up, down, level):
-                # Sprites du personnage
-        self.right = pygame.image.load(right).convert_alpha()
-        self.left = pygame.image.load(left).convert_alpha()
-        self.up = pygame.image.load(up).convert_alpha()
-        self.down = pygame.image.load(down).convert_alpha()
-        # Position du personnage en squares et en pixels
-        self.square_x = 0
-        self.square_y = 0
-        self.x = 0
-        self.y = 0
-        # Direction par défaut
-        self.axe = self.right
-        # Niveau dans lequel le personnage se trouve
-        self.level = level
-
-
-def move(self, axe):
-    """"move the character"""
-    if axe == 'right':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.right
-
-    if axe == 'left':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.left
-
-    if axe == 'up':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.up
-
-    if axe == 'down':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.down
-
-
 pygame.init()
 
 # VAR
@@ -121,14 +70,3 @@ while game:
     for event in pygame.event.get():
         if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
             game = False
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                game = False
-            elif event.key == K_RIGHT:
-                dk.deplacer('right')
-            elif event.key == K_LEFT:
-                dk.deplacer('left')
-            elif event.key == K_UP:
-                dk.deplacer('up')
-            elif event.key == K_DOWN:
-                dk.deplacer('down')
