a2cd6e0cd7d30fab43efdf9824a5b67df8af8c64
Amelien Boussard <amelien.boussard@gmail.com>
2018-03-29 11:07:17 +0200

    macgyver can move (only the sprite :sweat_smile:)

--- display.py
+++ display.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
 import pygame
 from pygame.locals import *
 from constant import *
@@ -12,20 +15,36 @@ window = pygame.display.set_mode((450, 450))
 ico = pygame.image.load(img_ico)
 level = Maze(maze)
 background = pygame.image.load(img_background).convert()
-img = Macgyver("images/macgyver_right.png", "images/macgyver_left.png",
-               "images/macgyver_up.png", "images/macgyver_down.png", level)
+mg = Macgyver("images/macgyver_right.png", "images/macgyver_left.png",
+              "images/macgyver_up.png", "images/macgyver_down.png", level)
 
 while game:
+
     pygame.time.Clock().tick(30)
     pygame.display.set_icon(ico)
     pygame.display.set_caption(title_window)
+
+    # For leave the program
+
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                mg.move('right')
+            elif event.key == K_LEFT:
+                mg.move('left')
+            elif event.key == K_UP:
+                mg.move('up')
+            elif event.key == K_DOWN:
+                mg.move('down')
+
     level.generate()
     window.blit(background, (0, 0))
     level.display(window)
+    window.blit(mg.axe, (mg.x, mg.y))
     # Refresh the display
     pygame.display.flip()
 
-    # For leave the program
-    for event in pygame.event.get():
-        if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-            game = False
+    if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+        game = False
+    if level.setting[mg.square_y][mg.square_x] == 'G':
+        game = False
--- macgyver.py
+++ macgyver.py
@@ -1,11 +1,13 @@
 import pygame
 from pygame.locals import *
+from constant import *
+
 
 class Macgyver:
     """Class for create the character Macgyver"""
 
     def __init__(self, right, left, up, down, level):
-                # Sprites of character
+        # Sprites of character
         self.right = pygame.image.load(right).convert_alpha()
         self.left = pygame.image.load(left).convert_alpha()
         self.up = pygame.image.load(up).convert_alpha()
@@ -14,39 +16,38 @@ class Macgyver:
         self.square_x = 0
         self.square_y = 0
         self.x = 0
-        self.y = 0
+        self.y = 30
         # Axe by default
         self.axe = self.right
         # Level in which the character is located
         self.level = level
 
+    def move(self, axe):
+        """"move the character"""
+        if axe == 'right':
+            if self.square_x < (nbr_sprite_square - 1):
+                if self.level.setting[self.square_y][self.square_x + 1] != '#':
+                    self.square_x += 1
+                    self.x = self.square_x * sprite_size
+                self.axe = self.right
 
-def move(self, axe):
-    """"move the character"""
-    if axe == 'right':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.right
+        if axe == 'left':
+            if self.square_x < (nbr_sprite_square - 1):
+                if self.level.setting[self.square_y][self.square_x + 1] != '#':
+                    self.square_x += 1
+                    self.x = self.square_x * sprite_size
+                self.axe = self.left
 
-    if axe == 'left':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.left
+        if axe == 'up':
+            if self.square_x < (nbr_sprite_square - 1):
+                if self.level.setting[self.square_y][self.square_x + 1] != '#':
+                    self.square_x += 1
+                    self.x = self.square_x * sprite_size
+                self.axe = self.up
 
-    if axe == 'up':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.up
-
-    if axe == 'down':
-        if self.square_x < (nbr_sprite_square - 1):
-            if self.level.setting[self.square_y][self.square_x + 1] != '#':
-                self.square_x += 1
-                self.x = self.square_x * sprite_size
-            self.axe = self.down
+        if axe == 'down':
+            if self.square_x < (nbr_sprite_square - 1):
+                if self.level.setting[self.square_y][self.square_x + 1] != '#':
+                    self.square_x += 1
+                    self.x = self.square_x * sprite_size
+                self.axe = self.down
